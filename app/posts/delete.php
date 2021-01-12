dfc46a2db49d4c89d8b42319379b8ba654fd9da7
aagneess <70575715+aagneess@users.noreply.github.com>
2021-01-12 17:06:32 +0100

    user can now delete posts

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-// In this file we delete new posts in the database.
+if (isset($_POST['post-id'])) {
+    $userId = (int) $_SESSION['user']['id'];
+    // $postId = getPostById($id, $pdo);
+    $postId = (int) $_POST['post-id'];
 
-redirect('/');
+    $statement = $pdo->prepare('DELETE FROM posts WHERE id = :postId AND user_id = :userId');
+    $statement->bindParam(':postId', $postId, PDO::PARAM_INT);
+    $statement->bindParam(':userId', $userId, PDO::PARAM_INT);
+
+
+    $statement->execute();
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    if ($statement) {
+        $_SESSION['message'] = 'You have successfully deleted the post!';
+    }
+}
+
+redirect('/../usernavigation/userposts.php');
