ea9543c795c50500ebad9933d665b49b752e34c6
aagneess <70575715+aagneess@users.noreply.github.com>
2021-01-12 01:18:16 +0100

    new design

--- usernavigation/post.php
+++ usernavigation/post.php
@@ -4,27 +4,25 @@
 
 <h2>Create new post</h2>
 
-<?php
-$user = getUserId($_SESSION['user']['id'], $pdo);
-$id = (int) $_SESSION['user']['id'];
-?>
 <form action="/../app/posts/store.php" method="post">
-    <div class="form-group">
-        <label for="title">Title</label>
-        <input class="form-control" type="title" name="title" id="title" placeholder="Title" value="" required>
-    </div>
+    <section class="create-post">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input class="form-control" type="title" name="title" id="title" placeholder="Title" value="" required>
+        </div>
 
-    <div class="form-group">
-        <label for="url">Link</label>
-        <input class="form-control" type="url" name="url" id="url" placeholder="https://..." required>
-    </div>
+        <div class="form-group">
+            <label for="url">Link</label>
+            <input class="form-control" type="url" name="url" id="url" placeholder="https://..." required>
+        </div>
 
-    <div class="form-group">
-        <label for="text_content">Write a Short Description!</label>
-        <textarea class="form-control" type="text_content" name="text_content" id="text_content" placeholder="..." required></textarea>
-    </div>
+        <div class="form-group">
+            <label for="text_content">Write a Short Description!</label>
+            <textarea class="form-control" type="text_content" name="text_content" id="text_content" placeholder="..." required></textarea>
+        </div>
 
-    <button type="submit" name="submit-post" class="btn btn-info">Submit</button>
+        <button type="submit" name="submit-post" class="btn btn-info">Submit</button>
+    </section>
 </form>
 
 
